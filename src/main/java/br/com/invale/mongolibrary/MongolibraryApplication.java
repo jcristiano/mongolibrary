46874b734560e39b0ccaad511e52ca73b23fe445
Joao Cristiano M Silva <jcmonsilv@gmail.com>
2021-10-28 21:39:58 -0300

    first commit

---
+++ src/main/java/br/com/invale/mongolibrary/MongolibraryApplication.java
@@ -0,0 +1,13 @@
+package br.com.invale.mongolibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongolibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongolibraryApplication.class, args);
+	}
+
+}
